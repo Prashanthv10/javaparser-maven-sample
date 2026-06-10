029894b7e2358ecb57e8a668c0edef75454a3570
Prashanthv10 <131243924+Prashanthv10@users.noreply.github.com>
2026-06-10 22:04:45 +0530

    Add Calculator class with addition methods

--- src/main/resources/dummy.java
+++ src/main/resources/dummy.java
@@ -1,1 +1,14 @@
-added this file delete it
+public class Calculator {
+
+    public int addNumbers(int a, int b) {
+        int result = a + b;
+        System.out.println("Result: " + result);
+        return result;
+    }
+
+    public int sumValues(int x, int y) {
+        int result = x + y;
+        System.out.println("Result: " + result);
+        return result;
+    }
+}
